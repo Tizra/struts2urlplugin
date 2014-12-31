2ac3751cd5a7f6811d77345063a9bc880bb5a2d2
David G. Durand <david.durand@tizra.com>
2014-12-31 12:59:29 -0500

    Initial commit of original svn r755

---
+++ README.md
@@ -0,0 +1,10 @@
+**Struts2 url plugin prerelease** 
+
+([Official project page](https://code.google.com/p/struts2urlplugin/))
+
+This is a git fork of the original project, which does not seem
+to be active (no changes since 2008). It's been useful to me,
+but as I've had to fix some issues, I've forked it onto github.
+The project page says Apache 2.0 license, so I have added license and copyright
+notices to the repo, since they were not in the SVN tree I originally
+checked out.
---
+++ struts2-url-plugin/src/test/java/com/blueskyminds/struts2/urlplugin/matcher/action/namespace/TestNamedVariableNamespaceMatcher.java
@@ -0,0 +1,53 @@
+package com.blueskyminds.struts2.urlplugin.matcher.action.namespace;
+
+import junit.framework.TestCase;
+import com.blueskyminds.struts2.urlplugin.matcher.MatchContext;
+
+/**
+ * Date Started: 21/02/2008
+ * <p/>
+ * History:
+ */
+public class TestNamedVariableNamespaceMatcher extends TestCase {
+
+    public void testNamespaceVariables() {
+        NamedVariableNamespaceMatcher matcher = new NamedVariableNamespaceMatcher();
+        MatchContext matchContext = new MatchContext();
+
+        assertTrue(matcher.match("", "", matchContext));
+        assertTrue(matcher.match("/", "/", matchContext));
+        assertTrue(matcher.match("/example", "/example", matchContext));
+        assertTrue(matcher.match("/example/", "/example/", matchContext));
+        assertTrue(matcher.match("/example/", "/{var1}/", matchContext));
+        assertEquals("example", matchContext.get("var1"));
+
+        matchContext = new MatchContext();
+        assertFalse(matcher.match("/example/", "/{var1}/{var2}/test/{var3}/", matchContext));
+        assertFalse(matcher.match("/au/nsw/test/", "/{var1}/{var2}/test/{var3}/", matchContext));
+        assertTrue(matcher.match("/au/nsw/test/test1/", "/{var1}/{var2}/test/{var3}/", matchContext));
+        assertEquals("au", matchContext.get("var1"));
+        assertEquals("nsw", matchContext.get("var2"));
+        assertEquals("test1", matchContext.get("var3"));
+    }
+
+    public void testNamespaceVariablesWithSlashes() {
+        NamedVariableNamespaceMatcher matcher = new NamedVariableNamespaceMatcher();
+        MatchContext matchContext = new MatchContext();
+
+        assertTrue(matcher.match("", "", matchContext));
+        assertTrue(matcher.match("/", "/", matchContext));
+        assertTrue(matcher.match("/example", "/example", matchContext));
+        assertTrue(matcher.match("/example/", "/example/", matchContext));
+        assertTrue(matcher.match("/example/", "/{var1}/", matchContext));
+        assertTrue(matcher.match("/example/", "/{var1*}/", matchContext));
+        assertEquals("example", matchContext.get("var1"));
+
+        matchContext = new MatchContext();
+        assertTrue(matcher.match("/au/nsw/test/", "/{var1}/{var2*}/", matchContext));
+        assertEquals("au", matchContext.get("var1"));
+        assertEquals("nsw/test", matchContext.get("var2"));
+        assertTrue(matcher.match("/au/nsw/test/test1/", "/{var1}/{var2*}/", matchContext));
+        assertEquals("au", matchContext.get("var1"));
+        assertEquals("nsw/test/test1", matchContext.get("var2"));
+    }
+}
